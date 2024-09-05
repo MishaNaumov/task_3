aa1ff4f4a9d09467aa0f135a51e86583dbe43285
Misha_Naumov <misha.naumov.999@gmail.com>
2024-09-05 23:23:55 +0300

    1

---
+++ pages/basic_auth_page.py
@@ -0,0 +1,15 @@
+from pages.base_page import BasePage
+from elements.label import Label
+
+
+class BasicAuthPage(BasePage):
+    RESULT_LOC = "//*[@id='content']//p"
+
+    def __init__(self, driver):
+        super().__init__(driver)
+
+        self.result_label = Label(self.driver, self.RESULT_LOC,
+                                   description="Login page -> Result label")
+
+    def get_text_result(self):
+        return self.result_label.get_text()
