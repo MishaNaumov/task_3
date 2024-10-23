afe051db9c3bb2ead886a629f140f842b369fb27
Misha_Naumov <misha.naumov.999@gmail.com>
2024-10-23 05:03:27 +0300

    final!!!!!!!

--- logger/logger.py
+++ logger/logger.py
@@ -6,9 +6,9 @@ class Logger:
     FILENAME = "my_logging.log"
     ENCODING = "utf-8"
     FORMAT = "%(levelname)s (%(asctime)s): %(message)s"
-    NAME_LOG = "log"
+    LOG_NAME = "log"
 
-    logger = logging.getLogger(NAME_LOG)
+    logger = logging.getLogger(LOG_NAME)
     logger.setLevel(LOG_LEVEL)
     handler = logging.FileHandler(
         filename=FILENAME,
--- pages/hovers_page.py
+++ pages/hovers_page.py
@@ -5,6 +5,9 @@ from elements.label import Label
 
 class HoversPage(BasePage):
     UNIQUE_LOC = "//*[@id='content']//h3"
+    IMG_WEB_ELEMENT_LOC = "//div[@class='figure'][{}]"
+    IMG_TEXT_LABEL = f"{IMG_WEB_ELEMENT_LOC}//h5"
+    IMG_A_LABEL_TEXT = f"{IMG_WEB_ELEMENT_LOC}//a"
 
     def __init__(self, driver):
         super().__init__(driver)
@@ -16,35 +19,35 @@ class HoversPage(BasePage):
         )
 
     def move_to_img(self, index):
-        img_web_element_loc = f"//div[@class='figure'][{index}]"
+        img_web_element_loc = self.IMG_WEB_ELEMENT_LOC.format(index)
 
         img_web_element = WebElement(
             self.driver,
             img_web_element_loc,
-            description="Hovers page -> IMG"
+            description=f"Hovers page -> IMG {index}"
         )
 
         img_web_element.move_to_element()
 
     def get_text_img(self, index):
-        img_text_label = f"//div[@class='figure'][{index}]//h5"
+        img_text_label = self.IMG_TEXT_LABEL.format(index)
 
         img_text_label = Label(
             self.driver,
             img_text_label,
-            description="Hovers page -> IMG text"
+            description=f"Hovers page -> IMG {index} text"
         )
 
         img_text_label.is_displayed()
         return img_text_label.get_text()
 
     def click_img(self, index):
-        img_a_label_text = f"//div[@class='figure'][{index}]//a"
+        img_a_label_text = self.IMG_A_LABEL_TEXT.format(index)
 
         img_a_label = Label(
             self.driver,
             img_a_label_text,
-            description="Hovers page -> IMG 3 href"
+            description=f"Hovers page -> IMG {index} href"
         )
 
         img_a_label.click()
